4ba3f8d1e7f782783a6b37d2f145538eb47c6bc9
allen7u@GitKraken <allen7u@gmail.com>
2022-08-30 17:12:41 +0800

    support both search results and playlist; and popup

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         获取用户所有视频链接(B站）
+// @name         B站搜索或列表链接
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://space.bilibili.com/*
+// @match        http://*/*
+// @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
 // ==/UserScript==
@@ -13,18 +14,71 @@
     'use strict';
 
     function copy_links_on_this_page(){
+
+        // get title
+        var title = document.title;
+
         var links = document.querySelectorAll('ul li a.title');
         var links_text = '';
+        var links_with_title = '';
         for(var i=0;i<links.length;i++){
             links_text += links[i].href + '\n';
+            links_with_title += '# '+links[i].innerText + '\n' + links[i].href + '\n';
         }
+        var combined_playlist = '\n#combined_playlist\n' + links_text + '\n' + links_with_title;
+
+        var search_results = document.querySelectorAll('.bili-video-card__info > div > a')
+        var search_results_text = '';
+        var search_results_with_title = '';
+        for(var i=0;i<search_results.length;i++){
+            search_results_text += search_results[i].href + '\n';
+            search_results_with_title += '# '+search_results[i].innerText + '\n' + search_results[i].href + '\n';
+        }
+        var combined_search_results = '\n#combined_search_results\n' + search_results_text + '\n' + search_results_with_title
+        
+        var combined_playlist_and_search_results = combined_playlist + '\n' + combined_search_results;
+        console.log(combined_playlist_and_search_results);
+
         // copy to clipboard
         var copyText = document.createElement('textarea')
-        copyText.value = links_text
+        
+        copyText.value = combined_playlist_and_search_results
         document.body.appendChild(copyText)
         copyText.select()
         document.execCommand('copy')
         document.body.removeChild(copyText)
+
+        console.log(combined_playlist_and_search_results);
+
+        // show message
+        if (links.length > 0 || search_results.length > 0) {
+        var message = document.createElement('div');
+        message.innerHTML = '<div class="alert alert-success" role="alert">' +
+            '<strong>Success!</strong> Copied to clipboard.' +  '</div>' +
+            combined_playlist_and_search_results
+        // add br to separate lines
+        message.innerHTML = message.innerHTML.replace(/\n/g, '<br>');
+        // fixed
+        message.style.position = 'fixed';
+        message.style.top = '25vh';
+        message.style.left = '25vw';
+        message.style.width = '50vw';
+        message.style.height = '75vh';
+        message.style.zIndex = '9999';
+        message.style.backgroundColor = '#fff';
+        message.style.borderRadius = '5px';
+        message.style.padding = '10px';
+        message.style.boxShadow = '0px 0px 10px #ccc';
+        message.style.fontSize = '16px';
+        message.style.overflow = 'auto';
+
+
+        document.body.appendChild(message);
+        setTimeout(function(){
+            document.body.removeChild(message);
+        }, 3000);
+        }
+        
 }
 
 
